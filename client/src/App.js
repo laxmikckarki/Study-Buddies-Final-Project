b78c1c94e9bb2235bd5c0189fd05b3233ff5475f
laxmi <lkc712628@gmail.com>
2023-06-24 21:56:09 +0100

    create register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import RegisterPage from "./pages/Register";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/register" element={<RegisterPage />} />
 		<Route path="/login" element={<Login />} />
 	</Routes>
 );
